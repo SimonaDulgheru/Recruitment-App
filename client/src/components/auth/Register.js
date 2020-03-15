ef2aa10b0418fd00b2d322b9506dbc0b4205f63e
Simona <s.c.dulgheru@gmail.com>
2020-03-15 01:06:14 +0000

    fixed errors alert

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,13 +1,10 @@
-
 //  eslint react/prop-types: 0 ;
 import React, { Fragment, useState } from "react";
 // import axios from "axios";
 //React/Redux package
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-
 import { register } from "../../actions/auth";
-
 import { setAlert } from "../../actions/alert";
 import PropTypes from "prop-types";
 
@@ -26,32 +23,32 @@ const Register = ({ setAlert, register }) => {
 	const onSubmit = async e => {
 		e.preventDefault();
 		if (password !== password2) {
-			setAlert("Passwords do not match", "danger");//App.css
+			setAlert("Passwords do not match", "danger"); //App.css
 		} else {
-			console.log('Registered')
-		// 	const newUser = {
-		// 		name,
-		// 		email,
-		// 		password
-		// 	};
+			console.log("Registered");
+			// 	const newUser = {
+			// 		name,
+			// 		email,
+			// 		password
+			// 	};
 
-		// 	try {
-		// 		const config = {
-		// 			headers: {
-		// 				"Content-Type": "application/json"
-		// 			}
-		// 		};
+			// 	try {
+			// 		const config = {
+			// 			headers: {
+			// 				"Content-Type": "application/json"
+			// 			}
+			// 		};
 
-		// 		const body = JSON.stringify(newUser);
+			// 		const body = JSON.stringify(newUser);
 
-		// 		const res = await axios.post("/api/users", body, config);
-		// 		console.log(res.data);
-		// 	} catch (err) {
-		// 		console.log(err.response.data);
-		// 	}
-		// 	console.log(formData);
-		// 	register({ name, email, password });
-		// }
+			// 		const res = await axios.post("/api/users", body, config);
+			// 		console.log(res.data);
+			// 	} catch (err) {
+			// 		console.log(err.response.data);
+			// 	}
+			// 	console.log(formData);
+			// 	register({ name, email, password });
+		}
 	};
 	return (
 		<Fragment>
@@ -120,16 +117,11 @@ Register.propTypes = {
 	setAlert: PropTypes.func.isRequired,
 	register: PropTypes.func.isRequired
 };
-const mapStateToProps = state => ({
-	isAuthenticated: state.auth.isAuthenticated
-  });
+// const mapStateToProps = state => ({
+// 	isAuthenticated: state.auth.isAuthenticated
+// });
 
 //Export Connect package
 //Connect takes in 2 parameters- one is Get state from alerts and second an obj with any actions we want to use(this case Alert)
 
-export default connect(
-	mapStateToProps,
-	{ setAlert, register }
-  )(Register);
-
-
+export default connect(null, { setAlert, register })(Register);
