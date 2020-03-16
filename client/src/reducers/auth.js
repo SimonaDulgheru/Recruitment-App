5062cf498abf1486c6f97450b106dd82b1f9ebcb
Neeraja Djearaman <neeru.jai@gmail.com>
2020-03-16 17:40:34 +0000

    html-register

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,45 +1,64 @@
 // recuder for auth
 import {
-	REGISTER_SUCCESS,
-	REGISTER_FAIL,
-	USER_LOADED,
-	AUTH_ERROR
-} from "../actions/types";
+    REGISTER_SUCCESS,
+    //REGISTER_FAIL,
+    USER_LOADED,
+    //AUTH_ERROR,
+    LOGIN_SUCCESS,
+    //LOGIN_FAIL,
+    LOGOUT,
+    ACCOUNT_DELETED
+} from '../actions/types';
 
 const initialState = {
-	token: localStorage.getItem("token"),
-	isAuthenticated: null,
-	loading: true,
-	user: null
+    token: localStorage.getItem('token'),
+    isAuthenticated: null,
+    loading: true,
+    user: null
 };
 
 export default function(state = initialState, action) {
-	const { type, userIdAuth } = action;
+    const { type, userIdAuth } = action;
 
-	switch (type) {
-		case USER_LOADED:
-			return {
-				...state,
-				isAuthenticated: true,
-				loading: false,
-				user: userIdAuth
-			};
-		case REGISTER_SUCCESS:
-			localStorage.setItem("token", userIdAuth.token);
-			return {
-				...state,
-				...userIdAuth,
-				isAuthenticated: true,
-				loading: false
-			};
-		case REGISTER_FAIL:
-		case AUTH_ERROR:
-			localStorage.removeItem("token");
-			return {
-				...state,
-				token: null,
-				isAuthenticated: false,
-				loading: false
-			};
-	}
+    switch (type) {
+        case USER_LOADED:
+            return {
+                ...state,
+                isAuthenticated: true,
+                loading: false,
+                user: userIdAuth
+            };
+        case REGISTER_SUCCESS:
+            return {
+                ...state,
+                ...userIdAuth,
+                isAuthenticated: true,
+                loading: false
+            };
+        case LOGIN_SUCCESS:
+            return {
+                ...state,
+                ...userIdAuth,
+                isAuthenticated: true,
+                loading: false
+            };
+        case ACCOUNT_DELETED:
+            return {
+                ...state,
+                token: null,
+                isAuthenticated: false,
+                loading: false,
+                user: null
+            };
+        case LOGOUT:
+            return {
+                ...state,
+                token: null,
+                isAuthenticated: false,
+                loading: false,
+                user: null
+            };
+        default:
+            return state;
+    }
 }
