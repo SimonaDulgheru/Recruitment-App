b773942fa8c538674e51187073331fffc826ad80
Neeraja Djearaman <neeru.jai@gmail.com>
2020-03-17 20:35:13 +0000

    token

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -1,59 +1,68 @@
 // recuder for auth
 import {
-	REGISTER_SUCCESS,
-	REGISTER_FAIL,
-	USER_LOADED,
-	AUTH_ERROR,
-	LOGIN_SUCCESS
-	// LOGIN_FAIL,
-	// LOGOUT,
-	// ACCOUNT_DELETED
+    REGISTER_SUCCESS,
+    REGISTER_FAIL,
+    USER_LOADED,
+    AUTH_ERROR,
+    LOGIN_SUCCESS,
+    LOGIN_FAIL,
+    // LOGOUT,
+    // ACCOUNT_DELETED
 } from "../actions/types";
 
 const initialState = {
-	token: localStorage.getItem("token"),
-	isAuthenticated: null,
-	loading: true,
-	user: null
+    token: localStorage.getItem("token"),
+    isAuthenticated: null,
+    loading: true,
+    user: null
 };
 
 export default function(state = initialState, action) {
-	const { type, userIdAuth } = action;
+    const { type, userIdAuth } = action;
 
-	switch (type) {
-		case USER_LOADED:
-			return {
-				...state,
-				isAuthenticated: true,
-				loading: false,
-				user: userIdAuth
-			};
-		case REGISTER_SUCCESS:
-			localStorage.setItem("token", userIdAuth.token);
-			return {
-				...state,
-				...userIdAuth,
-				isAuthenticated: true,
-				loading: false
-			};
-		case REGISTER_FAIL:
-		case AUTH_ERROR:
-			localStorage.removeItem("token");
-			return {
-				...state,
-				token: null,
-				isAuthenticated: false,
-				loading: false
-			};
-		case LOGIN_SUCCESS:
-			return {
-				...state,
-				...userIdAuth,
-				isAuthenticated: true,
-				loading: false
-			};
+    switch (type) {
+        case USER_LOADED:
+            return {
+                ...state,
+                isAuthenticated: true,
+                loading: false,
+                user: userIdAuth
+            };
+        case REGISTER_SUCCESS:
+            localStorage.setItem("token", userIdAuth.token);
+            return {
+                ...state,
+                ...userIdAuth,
+                isAuthenticated: true,
+                loading: false
+            };
+        case REGISTER_FAIL:
+        case AUTH_ERROR:
+            localStorage.removeItem("token");
+            return {
+                ...state,
+                token: null,
+                isAuthenticated: false,
+                loading: false
+            };
+        case LOGIN_SUCCESS:
+            localStorage.setItem("token", userIdAuth.token);
+            return {
+                ...state,
+                ...userIdAuth,
+                isAuthenticated: true,
+                loading: false
+            };
+        case LOGIN_FAIL:
+            localStorage.removeItem("token");
+            return {
+                ...state,
+                token: null,
+                isAuthenticated: false,
+                loading: false
+            };
 
-		default:
-			return state;
-	}
+        default:
+            return state;
+    }
 }
