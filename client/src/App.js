af56d9db0a644bafed3c966c17dc7bbc7b2d1727
Neeraja Djearaman <neeru.jai@gmail.com>
2020-03-18 12:25:34 +0000

    route to dashboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,54 @@
-import React, { Fragment, useEffect } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Landing from "./components/layout/Landing";
+import React, { Fragment, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Landing from './components/layout/Landing';
 
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import Alert from "./components/layout/Alert";
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
+import Navbar from './components/layout/Navbar';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
+
 //Redux
-import { Provider } from "react-redux";
-import store from "./store";
+import { Provider } from 'react-redux';
+import store from './store';
 
-import { loadUser } from "./actions/auth";
+import { loadUser } from './actions/auth';
 
-import userToken from "./utils/userToken";
+import userToken from './utils/userToken';
 
-import "./App.css";
+import './App.css';
 // import Navbar from "./components/layout/Navbar";
 
 if (localStorage.token) {
-	userToken(localStorage.token);
+  userToken(localStorage.token);
 }
 //https://reactjs.org/docs/hooks-effect.html  ckeck Note about empty array
 
 const App = () => {
-	useEffect(() => {
-		// Only runs once when is loaded ([])
-		// https://reactjs.org/docs/hooks-effect.html - check Note in docs
-		store.dispatch(loadUser());
-	}, []);
+  useEffect(() => {
+    // Only runs once when is loaded ([])
+    // https://reactjs.org/docs/hooks-effect.html - check Note in docs
+    store.dispatch(loadUser());
+  }, []);
 
-	return (
-		<Provider store={store}>
-			<Router>
-				<Fragment>
-					<Route exact path='/' component={Landing} />
-					<section className='container'>
-						<Alert />
-						<Switch>
-							<Route
-								exact
-								path='/register'
-								component={Register}
-							/>
-							<Route exact path='/login' component={Login} />
-						</Switch>
-					</section>
-				</Fragment>
-			</Router>
-		</Provider>
-	);
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Route exact path='/' component={Landing} />
+          <section className='container'>
+            <Alert />
+            <Switch>
+              <Route exact path='/register' component={Register} />
+              <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
+            </Switch>
+          </section>
+        </Fragment>
+      </Router>
+    </Provider>
+  );
 };
 
 export default App;
