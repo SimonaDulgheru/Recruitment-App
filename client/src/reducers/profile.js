2e92ec2c18b02ed328c7a04eb99c23c9be785574
Neeraja Djearaman <neeru.jai@gmail.com>
2020-03-18 22:40:30 +0000

    added links to dashboard

---
+++ client/src/reducers/profile.js
@@ -0,0 +1,47 @@
+import {
+    GET_PROFILE,
+    PROFILE_ERROR,
+    CLEAR_PROFILE,
+    UPDATE_PROFILE,
+    GET_PROFILES,
+    GET_REPOS
+} from '../actions/types';
+
+
+const initialState = {
+    profile: null,
+    profiles: [],
+    repos: [],
+    loading: true,
+    error: {}
+}
+
+export default function(state = initialState, action) {
+    const { type, userIdAuth } = action;
+
+    switch (type) {
+        case GET_PROFILE:
+
+            return {
+                ...state,
+                profile: userIdAuth,
+                loading: false
+            }
+        case PROFILE_ERROR:
+            return {
+                ...state,
+                error: userIdAuth,
+                loading: false
+            };
+        case CLEAR_PROFILE:
+            return {
+                ...state,
+                profile: null,
+                repos: [],
+                loading: false
+            }
+        default:
+            return state;
+
+    }
+}
