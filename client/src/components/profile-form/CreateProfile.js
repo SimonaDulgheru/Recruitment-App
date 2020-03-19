32e8f64208e20d7bf730ea02b17eff0e321a579b
Neeraja Djearaman <neeru.jai@gmail.com>
2020-03-19 12:35:40 +0000

    user profile inputs

--- client/src/components/profile-form/CreateProfile.js
+++ client/src/components/profile-form/CreateProfile.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, Fragment } from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import { Link, withRouter, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { createProfile } from '../../actions/profile';
@@ -97,7 +97,7 @@ const CreateProfile = ({ createProfile, history }) => {
             value={location}
             onChange={e => onChange(e)}
           />
-          <small className='form-text'>City,Country (eg.London,Uk)</small>
+          <small className='form-text'>City,Country (eg.London,UK)</small>
         </div>
         <div className='form-group'>
           <input
@@ -135,66 +135,74 @@ const CreateProfile = ({ createProfile, history }) => {
         </div>
 
         <div className='my-2'>
-          <button type='button' className='btn btn-light'>
+          <button
+            onClick={() => toggleSocialInputs(!displaySocialInputs)}
+            type='button'
+            className='btn btn-light'
+          >
             Your Social Network Links
           </button>
           <span>Optional</span>
         </div>
+        {displaySocialInputs && (
+          <Fragment>
+            <div className='form-group social-input'>
+              <i className='fab fa-twitter fa-2x'></i>
+              <input
+                type='text'
+                placeholder='Twitter URL'
+                name='twitter'
+                value={twitter}
+                onChange={e => onChange(e)}
+              />
+            </div>
 
-        <div className='form-group social-input'>
-          <i className='fab fa-twitter fa-2x'></i>
-          <input
-            type='text'
-            placeholder='Twitter URL'
-            name='twitter'
-            value={twitter}
-            onChange={e => onChange(e)}
-          />
-        </div>
+            <div className='form-group social-input'>
+              <i className='fab fa-facebook fa-2x'></i>
+              <input
+                type='text'
+                placeholder='Facebook URL'
+                name='facebook'
+                value={facebook}
+                onChange={e => onChange(e)}
+              />
+            </div>
 
-        <div className='form-group social-input'>
-          <i className='fab fa-facebook fa-2x'></i>
-          <input
-            type='text'
-            placeholder='Facebook URL'
-            name='facebook'
-            value={facebook}
-            onChange={e => onChange(e)}
-          />
-        </div>
+            <div className='form-group social-input'>
+              <i className='fab fa-youtube fa-2x'></i>
+              <input
+                type='text'
+                placeholder='YouTube URL'
+                name='youtube'
+                value={youtube}
+                onChange={e => onChange(e)}
+              />
+            </div>
 
-        <div className='form-group social-input'>
-          <i className='fab fa-youtube fa-2x'></i>
-          <input
-            type='text'
-            placeholder='YouTube URL'
-            name='youtube'
-            value={youtube}
-            onChange={e => onChange(e)}
-          />
-        </div>
+            <div className='form-group social-input'>
+              <i className='fab fa-linkedin fa-2x'></i>
+              <input
+                type='text'
+                placeholder='Linkedin URL'
+                name='linkedin'
+                value={linkedin}
+                onChange={e => onChange(e)}
+              />
+            </div>
 
-        <div className='form-group social-input'>
-          <i className='fab fa-linkedin fa-2x'></i>
-          <input
-            type='text'
-            placeholder='Linkedin URL'
-            name='linkedin'
-            value={linkedin}
-            onChange={e => onChange(e)}
-          />
-        </div>
+            <div className='form-group social-input'>
+              <i className='fab fa-instagram fa-2x'></i>
+              <input
+                type='text'
+                placeholder='Instagram URL'
+                name='instagram'
+                value={instagram}
+                onChange={e => onChange(e)}
+              />
+            </div>
+          </Fragment>
+        )}
 
-        <div className='form-group social-input'>
-          <i className='fab fa-instagram fa-2x'></i>
-          <input
-            type='text'
-            placeholder='Instagram URL'
-            name='instagram'
-            value={instagram}
-            onChange={e => onChange(e)}
-          />
-        </div>
         <input type='submit' className='btn btn-primary my-1' />
         <a className='btn btn-light my-1' href='dashboard.html'>
           Go Back
@@ -207,5 +215,10 @@ const CreateProfile = ({ createProfile, history }) => {
 CreateProfile.propTypes = {
   createProfile: PropTypes.func.isRequired
 };
+const mapStateToProps = state => ({
+  profile: state.profile
+});
 
-export default connect(null, { createProfile })(withRouter(CreateProfile));
+export default connect(mapStateToProps, { createProfile })(
+  withRouter(CreateProfile)
+);
