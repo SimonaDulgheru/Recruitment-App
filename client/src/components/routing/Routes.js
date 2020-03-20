f4075452a9ee9bcab0c4019fc5f3b787638a8f59
Neeraja Djearaman <neeru.jai@gmail.com>
2020-03-20 09:03:50 +0000

    routes file separated and navbar fixed

---
+++ client/src/components/routing/Routes.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Register from '../auth/Register';
+import Login from '../auth/Login';
+import Alert from '../layout/Alert';
+import Dashboard from '../dashboard/Dashboard';
+import CreateProfile from '../profile-form/CreateProfile';
+import EditProfile from '../profile-form/EditProfile';
+
+import PrivateRoute from './PrivateRoute';
+
+const Routes = () => {
+  return (
+    <section className='container'>
+      <Alert />
+      <Switch>
+        <Route exact path='/register' component={Register} />
+        <Route exact path='/login' component={Login} />
+
+        <Route exact path='/dashboard' component={Dashboard} />
+        <Route exact path='/create-profile' component={CreateProfile} />
+        <Route exact path='/edit-profile' component={EditProfile} />
+      </Switch>
+    </section>
+  );
+};
+
+export default Routes;
